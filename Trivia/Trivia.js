a65dcff227436202a27522c8f701e7870b1920ce
jhonvo <jhon26victor@gmail.com>
2021-11-17 14:53:21 -0500

    Trivia - Basic requirements

---
+++ Trivia/Trivia.js
@@ -0,0 +1,54 @@
+$(document).ready(() => {
+    $("#questions").hide();
+    search();
+});
+
+function search() {
+    new Promise((resolve, reject) => {
+    $.get({
+        url: `https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=boolean`,
+        success: resolve,
+        error: reject,
+    });
+    })
+    .then(createQuestions)
+    .catch(error => {
+        
+        console.log(`Something went wrong...`);
+    });
+}
+
+function createQuestions(result) {
+    console.log("this is results", result.results );
+    $("#questions").empty().show()
+    for (let i=0; i < result.results.length ; i++){
+        $("#questions").append(`
+            <div class="question q${i} mb-3">
+            <p>${result.results[i].question}</p>
+            <form>
+            <div class="form-check">
+            <input class="form-check-input" type="radio" name="question${i}" id="true">
+            <label class="form-check-label" for="true">True</label>
+            </div>
+            <div class="form-check">
+            <input class="form-check-input" type="radio" name="question${i}" id="false" >
+            <label class="form-check-label" for="false">False</label>
+            </div>
+            </div>
+            </form>
+        `)
+    }
+    // const element = `
+    // <div #id='card' class='card'>
+    //     <img class='img avatar' src="${user.avatar_url}" alt="avatar">
+    //     <p>Login: ${user.login}</p>
+    //     <p>Name: ${user.name || 'N/A'}</p>
+    //     <p>Repos: ${user.public_repos}</p>
+    //     <p>Gists: ${user.public_gists}</p>
+    // </div>
+    // `;
+
+    // $('.user-content').first().empty().show().append(element);
+}
+
+
